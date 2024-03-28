eade386433e56bf5300a0855a07eed3b72ae6956
Kwonsoonkyu <nerochaos1@gmail.com>
2024-03-28 20:54:57 +0900

    <feat>: 셀레니움 로그 비활성화
    - 셀레니움 로그 비활성화
    - 에러 출력
    - 페이지 이동 후 대기시간 0.2초 -> 1초로 변경

--- crawl_all_gameresult.py
+++ crawl_all_gameresult.py
@@ -9,51 +9,60 @@ options = webdriver.ChromeOptions()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--log-level=3")
+options.add_argument("--disable-loging")
 driver = webdriver.Chrome(options=options)
 
-SEASON_KEY = "S43G01N"
+SEASON_KEY = "S41G01N"
 
 key_list = [f"{SEASON_KEY}{i}" for i in range(1, 271)]
 results = pd.DataFrame()
 
 for key in key_list:
-    url = f"https://www.kbl.or.kr/game/record/{key}"
-    driver.get(url)
-    sleep.sleep(0.2)
+    try:
+        url = f"https://www.kbl.or.kr/game/record/{key}"
+        driver.get(url)
+        sleep.sleep(1)
 
-    date = driver.find_elements(By.XPATH, '//*[@id="container"]/div/ul[1]/li[2]/ul/li[2]/span')[0].text
-    date = date[:10]  # 2024.03.28 (목)
+        date = driver.find_elements(By.XPATH, '//*[@id="container"]/div/ul[1]/li[2]/ul/li[2]/span')[0].text
+        date = date[:10]  # 2024.03.28 (목)
 
-    player_detail_record = driver.find_elements(By.XPATH, '//*[@id="container"]/div/ul[2]/li[2]')[0]
-    player_detail_record.click()
-    sleep.sleep(0.2)
+        player_detail_record = driver.find_elements(By.XPATH, '//*[@id="container"]/div/ul[2]/li[2]')[0]
+        player_detail_record.click()
+        sleep.sleep(1)
 
-    home_team_xpath = '//*[@id="container"]/div/div[3]/div/div[2]/div[1]/div[1]/div/h6'
-    home_team = driver.find_elements(By.XPATH, home_team_xpath)[0].text
-    home_players = driver.find_elements(By.XPATH, '//*[@id="container"]/div/div[5]/div[2]/div[1]')[0]
-    home_players = pd.read_html(home_players.get_attribute("innerHTML"))[0]
+        home_team_xpath = '//*[@id="container"]/div/div[3]/div/div[2]/div[1]/div[1]/div/h6'
+        home_team = driver.find_elements(By.XPATH, home_team_xpath)[0].text
+        home_players = driver.find_elements(By.XPATH, '//*[@id="container"]/div/div[5]/div[2]/div[1]')[0]
+        home_players = pd.read_html(home_players.get_attribute("innerHTML"))[0]
 
-    home_records = driver.find_elements(By.XPATH, '//*[@id="container"]/div/div[5]/div[2]/div[2]')[0]
-    home_records = pd.read_html(home_records.get_attribute("innerHTML"))[0]
+        home_records = driver.find_elements(By.XPATH, '//*[@id="container"]/div/div[5]/div[2]/div[2]')[0]
+        home_records = pd.read_html(home_records.get_attribute("innerHTML"))[0]
 
-    home_records = pd.concat([home_players, home_records], axis=1)
-    home_records["Team"] = home_team
-    home_records["Date"] = date
+        home_records = pd.concat([home_players, home_records], axis=1)
+        home_records["Team"] = home_team
+        home_records["Date"] = date
 
-    away_team_xpath = '//*[@id="container"]/div/div[3]/div/div[2]/div[1]/div[3]/div/h6'
-    away_team = driver.find_elements(By.XPATH, away_team_xpath)[0].text
-    away_players = driver.find_elements(By.XPATH, '//*[@id="container"]/div/div[6]/div[2]/div[1]')[0]
-    away_players = pd.read_html(away_players.get_attribute("innerHTML"))[0]
+        away_team_xpath = '//*[@id="container"]/div/div[3]/div/div[2]/div[1]/div[3]/div/h6'
+        away_team = driver.find_elements(By.XPATH, away_team_xpath)[0].text
+        away_players = driver.find_elements(By.XPATH, '//*[@id="container"]/div/div[6]/div[2]/div[1]')[0]
+        away_players = pd.read_html(away_players.get_attribute("innerHTML"))[0]
 
-    away_records = driver.find_elements(By.XPATH, '//*[@id="container"]/div/div[6]/div[2]/div[2]')[0]
-    away_records = pd.read_html(away_records.get_attribute("innerHTML"))[0]
+        away_records = driver.find_elements(By.XPATH, '//*[@id="container"]/div/div[6]/div[2]/div[2]')[0]
+        away_records = pd.read_html(away_records.get_attribute("innerHTML"))[0]
 
-    away_records = pd.concat([away_players, away_records], axis=1)
-    away_records["Team"] = away_team
-    away_records["Date"] = date
+        away_records = pd.concat([away_players, away_records], axis=1)
+        away_records["Team"] = away_team
+        away_records["Date"] = date
 
-    result = pd.concat([home_records, away_records])
-    results = pd.concat([results, result])
+        result = pd.concat([home_records, away_records])
+        results = pd.concat([results, result])
+    except Exception as e:
+        print("-" * 10, date, "-" * 10)
+        print(key, e)
+        print(home_records)
+        print("-" * 30)
+        print()
 
 results.columns = [col[0] if col[0] == col[1] else f"{col[0]} {col[1]}" for col in results.columns]
 results.columns = [col.strip() for col in results.columns]
